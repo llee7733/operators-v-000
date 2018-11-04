866b7ea943f7abc657964a34dd7ee646db1956ca
Lloyd Lee <kinglloydlee@gmail.com>
2018-11-04 19:45:58 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+if speed < 40 || speed > 60
+  TRUE
+elsif speed >= 40 && speed <= 60
+  FALSE
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
